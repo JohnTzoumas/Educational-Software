2a9ad7dcd0a7bf8f4122cb5244746904007adf6c
George Antoniadis <g.antoniadis@datamation.gr>
2018-06-08 23:27:27 +0300

    Setting up thymleaf fragments

--- src/main/java/com/atl/edusoftware/data/entity/User.groovy
+++ src/main/java/com/atl/edusoftware/data/entity/User.groovy
@@ -23,35 +23,12 @@ class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
-    def id
+    int id
 
     @Column(name = "email")
     @Email(message = "Please provide a valid Email address")
     @NotEmpty(message = "Please provide an email")
-    def email
-
-    @Column(name = "password")
-    @Length(min = 3, message = "Password must be at least 3 characters")
-    @NotEmpty(message = "Please provide a password")
-    def password
-
-    @Column(name = "name")
-    @Length(min = 2, message = "Name must be at least 2 characters")
-    @NotEmpty(message = "Please provide a name")
-    def name
-
-    @Column(name = "surname")
-    @Length(min = 2, message = "Surname must be at least 2 characters")
-    @NotEmpty(message = "Please provide a Surname")
-    def surname
-
-    @Column(name = "age")
-    @NotNull(message = "Please provide an age")
-    def age
-
-    @Column(name = "role")
-    @NotEmpty(message = "Please provide a role")
-    def role
+    String email
 
 }
 
